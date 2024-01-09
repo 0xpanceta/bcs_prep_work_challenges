83d22ce061f17a157d6dd9bec901452e574e7f1a
Julio <julio.tarraf@gmail.com>
2024-01-09 09:53:15 -0300

    made multilanguageHello

---
+++ multilanguageHello/multilanguageHello.js
@@ -0,0 +1,57 @@
+// write a function which takes two argument: name and the language and it
+// returns a personalized greeting in the selected language 
+// If no language is provided it defaults to English. 
+
+// english: 'Welcome',
+// czech: 'Vitejte',
+// danish: 'Velkomst',
+// dutch: 'Welkom',
+// estonian: 'Tere tulemast',
+// finnish: 'Tervetuloa',
+// flemish: 'Welgekomen',
+// french: 'Bienvenue',
+// german: 'Willkommen',
+// irish: 'Failte',
+// italian: 'Benvenuto',
+// latvian: 'Gaidits',
+// lithuanian: 'Laukiamas',
+// polish: 'Witamy',
+// spanish: 'Bienvenido',
+// swedish: 'Valkommen',
+// welsh: 'Croeso'
+
+// example:
+// greeting("bob", "spanish") 
+// returns 
+// 'Bienvenido, bob'
+
+function greeting (name, language = 'english') {
+    const greetings = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'
+    };
+
+    let greeting = greetings[language.toLowerCase()];
+    if (!greeting) {
+        greeting = greetings['english'];
+    }
+
+    return `${greeting}, ${name}`;
+}
+
+console.log(greeting("julio", "SweDish"))
