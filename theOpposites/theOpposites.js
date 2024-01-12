14bcd1c98896374ea5c0e92cd0e25a69a9f2d0e5
Julio <julio.tarraf@gmail.com>
2024-01-12 10:32:22 -0300

    theOpposites returns 0 instead of -0

--- theOpposites/theOpposites.js
+++ theOpposites/theOpposites.js
@@ -6,9 +6,11 @@
 // returns 5
 
 function oppositeNum (number) {
-    if (typeof number !== `number` || isNaN(number)) {return "Please input valid number"}
+    if (typeof number !== `number` || isNaN(number)) {return "Please input valid number"} if (number == 0) {return 0}
     return number * (-1)
 }
 
 console.log(oppositeNum(10))
 console.log(oppositeNum(-5))
+console.log(oppositeNum(0))
+console.log(0*(-1))
